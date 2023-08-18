3b742495b167dcec87e94640cd3c29aa25b76916
JLPLabs <4744024+JLPLabs@users.noreply.github.com>
2023-08-18 16:23:52 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,5 +7,14 @@ make all test
 # put src/lua on the path
 echo "path+=$HOME/devops/lua-5.4.6/src" > $HOME/.zshenv
 
+# get luatweetnacl
+curl -L  -o luatweetnacl.master.zip http://github.com/philanc/luatweetnacl/zipball/master/
+unzip luatweetnacl.master.zip
+LUADIR=$HOME/devops/lua-5.4.6/src
+LUA=$HOME/devops/lua-5.4.6/src
+LUAINC=$HOME/devops/lua-5.4.6/src
+
+cd philanc-luatweetnacl-9507d35
+
 # launch new window (macos)
 osascript -e 'tell app "Terminal" to do script "echo $path"'
