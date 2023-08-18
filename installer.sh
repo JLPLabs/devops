8d4cf2135e2bcc8b9b710144b1d8f3c19cce73ce
JLPLabs <4744024+JLPLabs@users.noreply.github.com>
2023-08-18 16:34:46 -0500

    Update installer.sh
    pass env vars to luatweetnacl make

--- installer.sh
+++ installer.sh
@@ -8,15 +8,13 @@ cd ..
 # put src/lua on the path
 echo "path+=$HOME/devops/lua-5.4.6/src" > $HOME/.zshenv
 
-# get luatweetnacl
+# get luatweetnacl and build
 curl -L  -o luatweetnacl.master.zip http://github.com/philanc/luatweetnacl/zipball/master/
 unzip luatweetnacl.master.zip
-LUADIR=$HOME/devops/lua-5.4.6/src
-LUA=$HOME/devops/lua-5.4.6/src
-LUAINC=$HOME/devops/lua-5.4.6/src
-
 cd philanc-luatweetnacl-9507d35
-make
+LUADIR=$HOME/devops/lua-5.4.6/src LUA=$HOME/devops/lua-5.4.6/src LUAINC=$HOME/devops/lua-5.4.6/src make
+
+
 
 # launch new window (macos)
 osascript -e 'tell app "Terminal" to do script "echo $path"'
