38af0143ebeb7982a11961c2931957612b1e2500
JLPLabs <4744024+JLPLabs@users.noreply.github.com>
2023-08-21 12:01:17 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -12,7 +12,7 @@ echo "path+=$HOME/devops/lua-5.4.6/src" > $HOME/.zshenv
 curl -L  -o luatweetnacl.master.zip http://github.com/philanc/luatweetnacl/zipball/master/
 unzip luatweetnacl.master.zip
 cd philanc-luatweetnacl-9507d35
-LUADIR=$HOME/devops/lua-5.4.6/src LUA=$HOME/devops/lua-5.4.6/src LUAINC=$HOME/devops/lua-5.4.6/src make
+LUADIR=$HOME/devops/lua-5.4.6/src LUA=$HOME/devops/lua-5.4.6/src/lua LUAINC=$HOME/devops/lua-5.4.6/src make
 
 ##### NOTE  #####
 # the make file luatweetnacl needs to be changed to link correctly on macos
